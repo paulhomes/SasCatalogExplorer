a4ff7b0058644b5918fc5c67b21708301eb43dc7
Paul Homes <paul.homes@metacoda.com>
2013-01-26 21:08:15 +1000

    Added support for keyboard input/navigation for controls in WPF windows that have been loaded via Windows Forms.

--- src/v4.2/CatalogExplorer.cs
+++ src/v4.2/CatalogExplorer.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using SAS.Tasks.Toolkit;
 using SAS.Shared.AddIns;
+using System.Windows.Forms.Integration;
 
 namespace SAS.Tasks.Examples.CatalogExplorer
 {
@@ -36,6 +37,11 @@ namespace SAS.Tasks.Examples.CatalogExplorer
         {
             CatalogExplorerControl c = new CatalogExplorerControl(Consumer);
     
+            // The following allows keyboard input/navigation with controls in WPF windows
+            // which have been loaded via Windows Forms.
+            // See http://weblogs.asp.net/jdanforth/archive/2008/07/29/open-a-wpf-window-from-winforms.aspx
+            ElementHost.EnableModelessKeyboardInterop(c);
+
             c.Show();
             return ShowResult.Canceled;
         }
